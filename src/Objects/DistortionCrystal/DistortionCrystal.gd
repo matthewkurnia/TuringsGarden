788f608f99f2f1eb7a13daf6dc25b74928eacaa4
Matthew Kurnia <matthewkurnia@gmail.com>
2020-06-16 19:17:17 +0700

    Distorsion Crystal streamlined, no longer needs base area abstraction. source viewport camera removed, now uses canvas transform instead

--- src/Objects/DistortionCrystal/DistortionCrystal.gd
+++ src/Objects/DistortionCrystal/DistortionCrystal.gd
@@ -3,8 +3,9 @@ extends Node2D
 
 const TARGET_VIEWPORT = preload("SourceViewport.tscn")
 
-var viewport
-var velo = Vector2()
+var viewport :Viewport
+onready var source = $Source
+onready var projection = $Projection
 
 
 func _ready():
@@ -12,22 +13,22 @@ func _ready():
 
 
 func _physics_process(delta):
-	$Projection.set_collision_polygon($Source.get_intersected_polygons())
-	self.global_position += velo
-	viewport.set_camera_position($Source.global_position)
-
-
-func set_velo(new_velo: Vector2) -> void:
-	velo = new_velo
+	projection.set_collision_polygon(source.get_intersected_polygons())
+	viewport.set_canvas_transform(Transform2D(0, 0.5*viewport.size - source.global_position))
+#	viewport.set_camera_position($Source.global_position)
 
 
 func initialize_viewport() -> void:
 	viewport = TARGET_VIEWPORT.instance()
 	var vp_rect = Rect2()
-	for point in $Source.get_collision_polygon():
+	for point in source.get_collision_polygon():
 		vp_rect = vp_rect.expand(point)
 	viewport.size = vp_rect.size
 	viewport.world_2d = get_viewport().world_2d
-	viewport.set_camera_position($Source.global_position)
-	Global.get_current_root().call_deferred("add_child", viewport)
-	$Projection.set_sprite_texture(viewport.get_texture())
+	viewport.set_camera_position(source.global_position)
+	get_tree().get_root().call_deferred("add_child", viewport)
+	projection.set_sprite_texture(viewport.get_texture())
+
+
+func _on_tree_exiting():
+	viewport.queue_free()
--- src/Objects/DistortionCrystal/SourceViewport.gd
+++ src/Objects/DistortionCrystal/SourceViewport.gd
@@ -9,8 +9,3 @@ extends Viewport
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-func set_camera_position(global_pos: Vector2) -> void:
-#	set_canvas_transform(Transform2D(0.0, -global_pos))
-	$Camera2D.global_position = global_pos
